6fdf1e90a10158fe2a5f8d6f1e7eb2f9a779aed4
whunmr <whunmr@gmail.com>
2017-05-31 16:42:27 +0800

    add pact broker support

--- pact-provider/src/test/java/com/example/PactVerifyTest.java
+++ pact-provider/src/test/java/com/example/PactVerifyTest.java
@@ -3,22 +3,21 @@ package com.example;
 import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
 import au.com.dius.pact.provider.junit.loader.PactFolder;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(PactRunner.class)
 @Provider("Foo_Provider")
-@PactFolder("pact-from-consumer/")  //@PactBroker(host = "localhost", port = "80")
+//@PactFolder("pact-from-consumer/")
+@PactBroker(host = "localhost", port = "8002")
 public class PactVerifyTest {
     private static ConfigurableApplicationContext application;
 
